dfe0e9a06742399837c5e73d8e57028a713c7240
NatashaProt <natalia.protosov@gmail.com>
2016-12-21 12:41:24 +0300

    project added

---
+++ cssharp-example/cssharp-example/MyFirstTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace cssharp_example
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://google.com/";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("q")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Пошук Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
